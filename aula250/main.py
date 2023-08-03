e447e844dbe5e066bb2b0d03f14294d46daa755f
Matheus Spindula <matheusluiz98@hotmail.com>
2023-08-02 21:09:20 -0300

    Fim modulo 5

--- aula250/main.py
+++ aula250/main.py
@@ -4,4 +4,6 @@ import uma_linha
 # print(uma_linha.__doc__)
 # print(uma_linha.__file__)
 # print(uma_linha.__name__)
-help(uma_linha)
+# import varias_linhas
+import documentando_funcoes
+help(documentando_funcoes)
---
+++ aula250/varias_linhas.py
@@ -0,0 +1,5 @@
+"""O que seu módulo faz
+Lorem indolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+incididunt, ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+nostrud exercitation ullamco laboris nisi ut aliquip ex.
+"""
