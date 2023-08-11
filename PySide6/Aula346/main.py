6c667e4727ffe4bcb5e098c4ebd37c40cb4383ec
C1326439 Matheus Luiz Oliveira Spindula <matheussopluiz@outlook.com>
2023-08-11 18:44:27 -0300

    Add buttons, info components to calculator

--- PySide6/Aula346/main.py
+++ PySide6/Aula346/main.py
@@ -4,7 +4,10 @@ from main_window import MainWindow
 from PySide6.QtGui import QIcon
 from PySide6.QtWidgets import QApplication, QLabel
 from variables import WINDOW_ICON_PATH
+from styles import setupTheme
 from display import Display
+from info import Info
+from buttons import Button, ButtonsGrid
 
 
 def temp_label(texto):
@@ -17,15 +20,32 @@ if __name__ == '__main__':
     # Cria a aplicação
     app = QApplication(sys.argv)
     window = MainWindow()
+    setupTheme()
 
     # Define o ícone
     icon = QIcon(str(WINDOW_ICON_PATH))
     window.setWindowIcon(icon)
     app.setWindowIcon(icon)
 
+    # Info
+    info = Info('')
+    window.addWidgetToVLayout(info)
+
     # Display
     display = Display()
-    window.addToVLayout(display)
+    window.addWidgetToVLayout(display)
+    display.setPlaceholderText('Teste')
+
+    # # Button
+    # button = Button('Texto do botão')
+    # window.addWidgetToVLayout(button)
+
+    # button2 = Button('Texto do botão')
+    # window.addWidgetToVLayout(button2)
+
+    # Grid
+    buttonsGrid = ButtonsGrid()
+    window.vLayout.addLayout(buttonsGrid)
 
     # Executa tudo
     window.adjustFixedSize()
