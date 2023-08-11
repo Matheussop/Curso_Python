3a6b77ecd00ab9b89120b2c887f0a568771064f9
C1326439 Matheus Luiz Oliveira Spindula <matheussopluiz@outlook.com>
2023-08-11 18:11:17 -0300

    First display in calculator

--- PySide6/Aula346/main.py
+++ PySide6/Aula346/main.py
@@ -2,8 +2,16 @@ import sys
 
 from main_window import MainWindow
 from PySide6.QtGui import QIcon
-from PySide6.QtWidgets import QApplication
+from PySide6.QtWidgets import QApplication, QLabel
 from variables import WINDOW_ICON_PATH
+from display import Display
+
+
+def temp_label(texto):
+    label = QLabel(texto)
+    label.setStyleSheet('font-size: 150px;')
+    return label
+
 
 if __name__ == '__main__':
     # Cria a aplicação
@@ -14,6 +22,10 @@ if __name__ == '__main__':
     icon = QIcon(str(WINDOW_ICON_PATH))
     window.setWindowIcon(icon)
     app.setWindowIcon(icon)
+
+    # Display
+    display = Display()
+    window.addToVLayout(display)
 
     # Executa tudo
     window.adjustFixedSize()
