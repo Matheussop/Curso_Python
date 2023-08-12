e6a03ead7c2effa82a98be9e51516a2fa63cf5cd
Matheus Spindula <matheussopluiz@outlook.com>
2023-08-11 22:43:19 -0300

    button equal functionality and equation logic

--- PySide6/Aula346/main.py
+++ PySide6/Aula346/main.py
@@ -28,7 +28,7 @@ if __name__ == '__main__':
     # Display
     display = Display()
     window.addWidgetToVLayout(display)
-    display.setPlaceholderText('Teste')
+    display.setPlaceholderText('Digite um número')
 
     # Grid
     buttonsGrid = ButtonsGrid(display, info)
